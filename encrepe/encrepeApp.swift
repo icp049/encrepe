6ff3ea622f476a2670371f09d5eb7979577ff633
Ian Pedeglorio <ianpepe2000@gmail.com>
2025-04-16 22:02:13 -0600

    Initial Commit

---
+++ encrepe/encrepeApp.swift
@@ -0,0 +1,17 @@
+//
+//  encrepeApp.swift
+//  encrepe
+//
+//  Created by Ian Pedeglorio on 2025-04-16.
+//
+
+import SwiftUI
+
+@main
+struct encrepeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
